be8a8254f5d3127b4da9c99900e3a4dfea3d6908
Sun <Zhuanz@MacBook-Pro.local>
2026-05-22 01:04:29 +0800

    modify init.sql 1 time

--- sql/init.sql
+++ sql/init.sql
@@ -11,7 +11,6 @@ create table if not exists subjects(
     is_activate int not null default 1, -- 1启用，0禁用
     created_at text not null default (datetime('now','localtime')),
     updated_at text not null default (datetime('now','localtime'))
-
 );
 
 -- 计划表 总/年/月/日，树形结构
@@ -30,9 +29,23 @@ create table if not exists plans(
     sort_order int not null default 0,
     created_at text not null default (datetime('now','localtime')),
     updated_at text not null default (datetime('now','localtime')),
-    foreign key (parent_id) references plans(id) on delete cascade,
-    foreign key (subject_id) references subjects(id) on delete set null
+    foreign key (parent_id) references plans(id) on delete cascade,-- 多级联删除，即删除父计划，就删除子计划
+    foreign key (subject_id) references subjects(id) on delete restrict-- 计划存在并引用该科目表，就不允许删除该科目
 );
 
+-- 数据库性能优化
 
---打卡记录表
+-- 打卡记录表
+create table if not exists punch_recodes(
+    id int primary key autoincrement,
+    punch_date text not null,--打卡日期，格式YYYY-MM-DD
+    subject_id int not null,
+    actual_minutes int not null default 0,
+    content text,
+    is_makeup int not null default 0,-- 1补卡 0正常
+    created_at text not null default (datetime('now','localtime')),
+    updated_at text not null default(datetime('now','localtime')),
+    foreign key (subject_id) references subjects(id) on delete restrict,-- 如果还有计划引用这个科目，就不许删除这个科目
+    unique(punch_date,subject_id)-- 联合约束，打卡日期和打卡的科目不能相同
+
+)
